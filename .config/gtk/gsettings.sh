81de291bfa24ebd4b1ecd2b54b44758e46908651
Casper <draakie@protonmail.com>
2022-08-30 18:11:13 +0200

    gtk: configure GTK theming through gsettings (working), settings.ini removed (not working)

---
+++ .config/gtk/gsettings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# GTK theme: https://www.gnome-look.org/p/1441725
+
+# Icon theme: https://store.kde.org/p/1405210
+# For icons: Download and install to `.local/share/icons`
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Skeuos-Blue-Dark'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code 11'
+gsettings set org.gnome.desktop.interface cursor-theme 'Simp1e'
+gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+
