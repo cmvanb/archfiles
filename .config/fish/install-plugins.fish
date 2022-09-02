589462cb4b2733d27e9fd24b11d2509211de6795
Casper <draakie@protonmail.com>
2022-09-02 16:05:05 +0200

    fish: add plugin installation script

---
+++ .config/fish/install-plugins.fish
@@ -0,0 +1,7 @@
+# Install plugin manager.
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# Install plugins.
+fisher install ilancosman/tide@v5
+fisher install jorgebucaran/autopair.fish
+
