5987235fee6babe601d21f8f898870bc7497a34b
Casper <draakie@protonmail.com>
2022-09-06 15:41:57 +0200

    nnn: Add preview-tui plugin, add color configuration, [WIP] Wezterm file previewer implementation.

--- .config/fish/functions/nnn.fish
+++ .config/fish/functions/nnn.fish
@@ -27,12 +27,32 @@ function nnn --wraps nnn --description 'support nnn quit and change directory'
     # stty lwrap undef
     # stty lnext undef
 
-    # TODO: Configure custom colors
-    # NNN_FCOLORS=0203040a07050608010d0b09
+    set -x NNN_PLUG "p:preview-tui"
+
+    set _block_device 02
+    set _char_device 03
+    set _directory 04
+    set _executable 0a
+    set _regular 07
+    set _hard_link 05
+    set _sym_link 06
+    set _missing_details 08
+    set _orphan_link 01
+    set _fifo 0d
+    set _socket 0b
+    set _unknown 09
+    set -x NNN_FCOLORS $_block_device$_char_device$_directory$_executable$_regular$_hard_link$_sym_link$_missing_details$_orphan_link$_fifo$_socket$_unknown
+
+    # set -x NNN_OPENER "$XDG_CONFIG_HOME/nnn/plugins/nuke"
 
     # The command function allows one to alias this function to `nnn` without
     # making an infinitely recursive alias
-    command nnn -d -U $argv
+
+    # `nnn` Options
+    # d: Show file details.
+    # U: Show user and group names in statusbar.
+    # a: Create a new FIFO path for each instance (enables multiple instances of nnn using FIFO).
+    command nnn -d -U -a $argv
 
     if test -e $NNN_TMPFILE
         source $NNN_TMPFILE
