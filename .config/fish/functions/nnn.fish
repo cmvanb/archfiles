2ad32395b99ae36083e82a0724ba0be8a49f73a6
Casper <draakie@protonmail.com>
2022-09-20 22:30:08 +0200

    nnn: Clear on exit, improve color scheme, basic wezterm integration for preview-tui.

--- .config/fish/functions/nnn.fish
+++ .config/fish/functions/nnn.fish
@@ -31,17 +31,19 @@ function nnn --wraps nnn --description 'support nnn quit and change directory'
 
     set _block_device 02
     set _char_device 03
-    set _directory 04
+    set _directory 0c
     set _executable 0a
     set _regular 07
     set _hard_link 05
-    set _sym_link 06
-    set _missing_details 08
+    set _sym_link 0e
+    set _missing_details 1f
     set _orphan_link 01
     set _fifo 0d
     set _socket 0b
     set _unknown 09
     set -x NNN_FCOLORS $_block_device$_char_device$_directory$_executable$_regular$_hard_link$_sym_link$_missing_details$_orphan_link$_fifo$_socket$_unknown
+
+    set -x NNN_COLORS '#0c0a0b0d'
 
     # set -x NNN_OPENER "$XDG_CONFIG_HOME/nnn/plugins/nuke"
 
@@ -53,6 +55,10 @@ function nnn --wraps nnn --description 'support nnn quit and change directory'
     # U: Show user and group names in statusbar.
     # a: Create a new FIFO path for each instance (enables multiple instances of nnn using FIFO).
     command nnn -d -U -a $argv
+
+    # NOTE: `nnn` leaves a messy terminal behind, so we `clear` it. This may be
+    # undesirable as we lose terminal scrollback...
+    clear
 
     if test -e $NNN_TMPFILE
         source $NNN_TMPFILE
