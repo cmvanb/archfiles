9375aa4d01ef472e3519656872557b251a1a981e
Casper <draakie@protonmail.com>
2022-09-29 16:22:40 +0200

    fish: Explicitly use ANSI colors for prompt.

--- .config/shell/fish/interactive.fish
+++ .config/shell/fish/interactive.fish
@@ -89,6 +89,8 @@ bind . 'expand-dot-to-double-dot'
 # Colors
 #-------------------------------------------------------------------------------
 
+# TODO: Use ANSI colors only.
+
 set -U fish_color_autosuggestion (color_named 'l1_gray')
 set -U fish_color_command (color_named 'l1_cyan')
 set -U fish_color_comment (color_named 'd1_gray')
@@ -124,36 +126,36 @@ set -U fish_color_valid_path (color_named 'debug') # --underline
 # Prompt (https://github.com/IlanCosman/tide)
 #-------------------------------------------------------------------------------
 
-set -U tide_prompt_color_separator_same_color (color_named 'white')
+set -U tide_prompt_color_separator_same_color (color_named 'ansi_white')
 set -U tide_left_prompt_items context time jobs git virtual_env pwd
 set -U tide_right_prompt_items
 
 set -U tide_context_always_display false
-set -U tide_context_bg_color (color_named 'd2_gray')
-set -U tide_context_color_default (color_named 'white')
-set -U tide_context_color_root (color_named 'red')
-set -U tide_context_color_ssh (color_named 'yellow')
+set -U tide_context_bg_color (color_named 'ansi_brblack')
+set -U tide_context_color_default (color_named 'ansi_white')
+set -U tide_context_color_root (color_named 'ansi_red')
+set -U tide_context_color_ssh (color_named 'ansi_yellow')
 
 set -U tide_time_format '%H:%M:%S'
-set -U tide_time_bg_color (color_named 'black')
-set -U tide_time_color (color_named 'cyan')
+set -U tide_time_bg_color (color_named 'ansi_black')
+set -U tide_time_color (color_named 'ansi_cyan')
 
 set -U tide_jobs_icon ''
-set -U tide_jobs_bg_color (color_named 'red')
-set -U tide_jobs_color (color_named 'black')
+set -U tide_jobs_bg_color (color_named 'ansi_red')
+set -U tide_jobs_color (color_named 'ansi_black')
 
 set -U tide_git_icon ''
-set -U tide_git_bg_color (color_named 'green')
-set -U tide_git_bg_color_unstable (color_named 'yellow')
-set -U tide_git_bg_color_urgent (color_named 'red')
+set -U tide_git_bg_color (color_named 'ansi_green')
+set -U tide_git_bg_color_unstable (color_named 'ansi_yellow')
+set -U tide_git_bg_color_urgent (color_named 'ansi_red')
 
 # TODO: Configure virtualenv module.
 
 set -U tide_pwd_icon ''
 set -U tide_pwd_icon_home ''
 set -U tide_pwd_icon_unwritable ''
-set -U tide_pwd_bg_color (color_named 'blue')
-set -U tide_pwd_color_anchors (color_named 'l1_white')
-set -U tide_pwd_color_dirs (color_named 'l1_white')
-set -U tide_pwd_color_truncated_dirs (color_named 'white')
+set -U tide_pwd_bg_color (color_named 'ansi_blue')
+set -U tide_pwd_color_anchors (color_named 'ansi_brwhite')
+set -U tide_pwd_color_dirs (color_named 'ansi_brwhite')
+set -U tide_pwd_color_truncated_dirs (color_named 'ansi_white')
 
