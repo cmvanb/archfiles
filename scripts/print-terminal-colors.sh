f64e50bbfbedb7a4cf8fabe15138a376da7b50fe
Casper <draakie@protonmail.com>
2022-09-29 16:24:05 +0200

    scripts: Improve terminal color printing.

--- scripts/print-terminal-colors.sh
+++ scripts/print-terminal-colors.sh
@@ -1,114 +1,114 @@
 #!/bin/bash
 
-# Tom Hale, 2016. MIT Licence.
-# Print out 256 colours, with each number printed in its corresponding colour
-# See http://askubuntu.com/questions/821157/print-a-256-color-test-pattern-in-the-terminal/821163#821163
+#-------------------------------------------------------------------------------
+# Print terminal colors and indices.
+#
+# NOTE: Color index assignments are defined by the terminal emulator.
+#
+# Copyright (c) 2016 Tom Hale
 
-set -eu # Fail on errors or undeclared variables
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#-------------------------------------------------------------------------------
 
-printable_colours=256
+# Fail on errors or undeclared variables.
+set -eu 
 
-# Return a colour that contrasts with the given colour
-# Bash only does integer division, so keep it integral
-function contrast_colour {
-    local r g b luminance
-    colour="$1"
+max_printable_colors=256
 
-    if (( colour < 16 )); then # Initial 16 ANSI colours
-        (( colour == 0 )) && printf "15" || printf "0"
+# Based on the input color index, return a contrasting color index.
+# NOTE: This is hard coded, no actual contrast/luminance calculation is done.
+function contrast_color {
+    color="$1"
+
+    # ANSI colors
+    if (( color < 16 )); then
+        (( color == 0 )) && printf "7" || printf "0"
         return
     fi
 
-    # Greyscale # rgb_R = rgb_G = rgb_B = (number - 232) * 10 + 8
-    if (( colour > 231 )); then # Greyscale ramp
-        (( colour < 244 )) && printf "15" || printf "0"
+    # Customized color bands
+    if (( color >= 16 )) && (( color < 106 )); then
+        band_base=$(( (color - 5) / 10 ))
+        band_max=$(( (band_base * 10) + 15 ))
+        band_index=$(( (color - 6) % 10 ))
+
+        (( band_index < 4 )) && printf "$band_max" || printf "0"
         return
     fi
 
-    # All other colours:
-    # 6x6x6 colour cube = 16 + 36*R + 6*G + B  # Where RGB are [0..5]
-    # See http://stackoverflow.com/a/27165165/5353461
-
-    # r=$(( (colour-16) / 36 ))
-    g=$(( ((colour-16) % 36) / 6 ))
-    # b=$(( (colour-16) % 6 ))
-
-    # If luminance is bright, print number in black, white otherwise.
-    # Green contributes 587/1000 to human perceived luminance - ITU R-REC-BT.601
-    (( g > 2)) && printf "0" || printf "15"
-    return
-
-    # Uncomment the below for more precise luminance calculations
-
-    # # Calculate percieved brightness
-    # # See https://www.w3.org/TR/AERT#color-contrast
-    # # and http://www.itu.int/rec/R-REC-BT.601
-    # # Luminance is in range 0..5000 as each value is 0..5
-    # luminance=$(( (r * 299) + (g * 587) + (b * 114) ))
-    # (( $luminance > 2500 )) && printf "0" || printf "15"
+    # Unused colors
+    printf "8"
 }
 
-# Print a coloured block with the number of that colour
-function print_colour {
-    local colour="$1" contrast
-    contrast=$(contrast_colour "$1")
-    printf "\e[48;5;%sm" "$colour"                # Start block of colour
-    printf "\e[38;5;%sm%3d" "$contrast" "$colour" # In contrast, print number
-    printf "\e[0m"                               # Reset colour
+# Print a colored block with the number of that color.
+function print_color {
+    local color="$1" index
+    index=$(contrast_color "$1")
+
+    # Color block
+    printf "\e[48;5;%sm" "$color"
+
+    # Contrasted index text
+    printf "\e[38;5;%sm%3d" "$index" "$color"
+
+    # Reset
+    printf "\e[0m"
 }
 
-# Starting at $1, print a run of $2 colours
-function print_run {
+# Starting at $1, print a band of $2 colors.
+function print_band {
     local i
-    for (( i = "$1"; i < "$1" + "$2" && i < printable_colours; i++ )) do
-        print_colour "$i"
+    for (( i = "$1"; i < "$1" + "$2" && i < max_printable_colors; i++ )) do
+        print_color "$i"
     done
     printf "  "
 }
 
-# Print blocks of colours
-function print_blocks {
-    local start="$1" i
-    local end="$2" # inclusive
-    local block_cols="$3"
-    local block_rows="$4"
-    local blocks_per_line="$5"
-    local block_length=$((block_cols * block_rows))
-
-    # Print sets of blocks
-    for (( i = start; i <= end; i += (blocks_per_line-1) * block_length )) do
-        printf "\n" # Space before each set of blocks
-        # For each block row
-        for (( row = 0; row < block_rows; row++ )) do
-            # Print block columns for all blocks on the line
-            for (( block = 0; block < blocks_per_line; block++ )) do
-                print_run $(( i + (block * block_length) )) "$block_cols"
-            done
-            (( i += block_cols )) # Prepare to print the next row
-            printf "\n"
-        done
-    done
-}
-
-print_run 8 8
+# Print ANSI colors.
+print_band 0 8
 printf "\n"
-print_run 0 8
-printf "\n"
-print_run 16 8
-printf "\n"
-print_run 24 8
-printf "\n"
+print_band 8 8
 printf "\n"
 
-print_colour 0
-print_colour 24
-print_colour 16
-print_colour 8
-print_colour 31
-print_colour 23
-print_colour 7
-print_colour 15
 printf "\n"
 
-print_blocks 16 231 6 6 2 # 6x6x6 colour cube between 16 and 231 inclusive
-print_blocks 232 255 12 2 1 # Not 50, but 24 Shades of Grey
+# Print customized color bands.
+print_band 16 10
+printf "\n"
+print_band 26 10
+printf "\n"
+print_band 36 10
+printf "\n"
+print_band 46 10
+printf "\n"
+print_band 56 10
+printf "\n"
+print_band 66 10
+printf "\n"
+print_band 76 10
+printf "\n"
+print_band 86 10
+printf "\n"
+print_band 96 10
+printf "\n"
+
+# Print unused colors.
+# print_band 106 255
+# printf "\n"
+
