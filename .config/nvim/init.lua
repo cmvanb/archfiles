8c4763104098036056575a681d894ac71f7c2d85
Casper <draakie@protonmail.com>
2022-10-13 00:31:22 +0200

    nvim: Be more precise with autocommands.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,44 +2,35 @@
 -- Neovim configuration
 --------------------------------------------------------------------------------
 
--- Helpers
---------------------------------------------------------------------------------
-
-local cmd = vim.cmd
-local opt = vim.opt
-local api = vim.api
-local fn = vim.fn
-local g = vim.g
-
 -- Automatic reload
 --------------------------------------------------------------------------------
 
 -- Reload configuration files automatically when edited. Also triggers package
 -- manager compile step.
-cmd([[
+vim.cmd([[
     augroup ReloadInit
         autocmd!
-        autocmd BufWritePost init.lua source <afile>
+        autocmd BufWritePost ~/.config/nvim/init.lua source <afile>
     augroup end
     augroup ReloadSettings
         autocmd!
-        autocmd BufWritePost settings.lua source <afile>
+        autocmd BufWritePost ~/.config/nvim/lua/settings.lua source <afile>
     augroup end
     augroup ReloadGUI
         autocmd!
-        autocmd BufWritePost gui.lua source <afile>
+        autocmd BufWritePost ~/.config/nvim/lua/gui.lua source <afile>
     augroup end
     augroup ReloadKeybinds
         autocmd!
-        autocmd BufWritePost keybinds.lua source <afile>
+        autocmd BufWritePost ~/.config/nvim/lua/keybinds.lua source <afile>
     augroup end
     augroup ReloadHighlights
         autocmd!
-        autocmd BufWritePost highlights.lua source <afile>
+        autocmd BufWritePost ~/.config/nvim/lua/highlights.lua source <afile>
     augroup end
     augroup ReloadPlugins
         autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+        autocmd BufWritePost ~/.config/nvim/lua/plugins.lua source <afile> | PackerCompile
     augroup end
 ]])
 
