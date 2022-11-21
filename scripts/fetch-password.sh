a117dc4475fb78008c8f1e0f39dc527657cf84b2
Casper <draakie@protonmail.com>
2022-11-21 17:41:43 +0100

    scripts: Improve fetch password script to be more resilient.

--- scripts/fetch-password.sh
+++ scripts/fetch-password.sh
@@ -1,79 +1,120 @@
 #!/bin/bash
+#-------------------------------------------------------------------------------
+# Fetch password from Bitwarden
+#-------------------------------------------------------------------------------
 
-# functions
+set -o nounset
+set -o pipefail
+
+# Functions
+#-------------------------------------------------------------------------------
 bw_abort ()
 {
-    # always lock and logout
+    # Always lock and logout.
     bw_finalize
-
     exit $1
 }
 
 bw_finalize ()
 {
     bw lock
-
     bw logout
 }
 
-# input validation
-if [[ -z $1 ]] ; then
-    echo 'Missing parameter: TARGET'
+# Input validation
+#-------------------------------------------------------------------------------
+if [[ -z $1 ]]; then
+    echo 'Missing parameter: TARGET' 1>&2
     exit 1
 fi
 
 TARGET=$1
 
-# retrieve secrets
+# Retrieve secrets
+#-------------------------------------------------------------------------------
 . ~/.secrets
 
-if [[ -z $BW_CLIENTID ]] ; then
-    echo 'Missing secret: BW_CLIENTID'
+if [[ -z $BW_CLIENTID ]]; then
+    echo 'Missing secret: BW_CLIENTID' 1>&2
     exit 2
 fi
 
-if [[ -z $BW_CLIENTSECRET ]] ; then
-    echo 'Missing secret: BW_CLIENTSECRET'
+if [[ -z $BW_CLIENTSECRET ]]; then
+    echo 'Missing secret: BW_CLIENTSECRET' 1>&2
     exit 3
 fi
 
+# Login to bitwarden CLI with personal API key
+#-------------------------------------------------------------------------------
 echo 'Login to Bitwarden'
 
-# login to bitwarden CLI with personal API key
-BW_CLIENTID=$BW_CLIENTID BW_CLIENTSECRET=$BW_CLIENTSECRET bw login --apikey --raw
+LOGIN_RESULT=$(BW_CLIENTID=$BW_CLIENTID BW_CLIENTSECRET=$BW_CLIENTSECRET bw login --apikey --raw)
 
-if [[ $? -ne 0 ]] ; then
+if [[ $? -ne 0 ]]; then
     bw_abort 4
 fi
 
-# require master password
-SESSION_KEY=$(bw unlock)
+# echo $LOGIN_RESULT
+# echo $LOGIN_RESULT =~ "You are already logged in"
+#
+# if [[ $? -ne 0 ]]; then
+#     if [[ LOGIN_RESULT =~ "You are already logged in" ]]; then
+#         echo 'Already logged in, continuing.'
+#     else
+#         echo $LOGIN_RESULT
+#         bw_abort 4
+#     fi
+# fi
 
-if [[ $? -ne 0 ]] ; then
+# Require master password
+#-------------------------------------------------------------------------------
+SESSION_KEY=$(bw unlock)
+if [[ $? -ne 0 ]]; then
     bw_abort 5
 fi
 
-# retrieve target password with session key
-# RESULT=$(bw get password $TARGET --session $SESSION_KEY)
-RESULT=$(bw get item $TARGET --session $SESSION_KEY)
-
-if [[ $? -ne 0 ]] ; then
+# Retrieve target password with session key
+#-------------------------------------------------------------------------------
+# RESULT=$(bw get item $TARGET --session $SESSION_KEY)
+RESULT=$(bw list items --search $TARGET --session $SESSION_KEY)
+if [[ $? -ne 0 ]]; then
     bw_abort 6
 fi
 
-NAME=$(echo $RESULT | jq -r '.name')
-PASSWORD=$(echo $RESULT | jq -r '.login.password')
+ITEMS_COUNT=$(echo $RESULT | jq '. | length')
 
-printf '\n'
-echo 'Found item: '$NAME
+if [[ $ITEMS_COUNT -gt 1 ]]; then
+    printf '\n'
+    echo "Found multiple items matching input: \`$TARGET\`." 1>&2
+    printf '\n'
+    echo "$(echo $RESULT | jq -r '.[] | {name: .name, id: .id} | values[]')" 1>&2
+    printf '\n'
 
-# copy result to clipboard so we can use it immediately
-wl-copy $PASSWORD
+    bw_abort 7
 
-printf '\n'
-echo 'Password is in Wayland clipboard'
-printf '\n'
+elif [[ $ITEMS_COUNT -lt 1 ]]; then
+    printf '\n'
+    echo "Did not find any items matching input: \`$TARGET\`." 1>&2
+    printf '\n'
 
-# always lock and logout
+    bw_abort 8
+
+elif [[ $ITEMS_COUNT -eq 1 ]]; then
+    NAME=$(echo $RESULT | jq -r '.[] | .name')
+    PASSWORD=$(echo $RESULT | jq -r '.[] | .login.password')
+
+    printf '\n'
+    echo 'Found item: '$NAME
+
+    # Copy result to clipboard so we can use it immediately
+    wl-copy $PASSWORD
+
+    printf '\n'
+    echo 'Password is in Wayland clipboard.'
+    printf '\n'
+fi
+
+# Always lock and logout
+#-------------------------------------------------------------------------------
 bw_finalize
 
