8aa0dff04f92bdc3b2be9595d5bac0346c47bc3b
Casper <draakie@protonmail.com>
2022-12-12 18:06:52 +0100

    scripts: Can now `kebab` directories!

--- scripts/rename-kebabcase.sh
+++ scripts/rename-kebabcase.sh
@@ -6,13 +6,15 @@ set -o pipefail
 
 . $HOME/scripts/name-formatting.sh
 
-SRC_FILENAME="${1%.*}"
-SRC_FILE_EXT="${1##*.}"
-
 if [[ -f "$1" ]]; then
+    SRC_FILENAME="${1%.*}"
+    SRC_FILE_EXT="${1##*.}"
     DST_FILENAME=$(convert_to_kebab_case "$SRC_FILENAME")".$SRC_FILE_EXT"
     mv "$1" "$DST_FILENAME"
+elif [[ -d "$1" ]]; then
+    DST_DIRNAME=$(convert_to_kebab_case "$1")
+    mv "$1" "$DST_DIRNAME"
 else
-    echo "File does not exist." 1>&2; exit 1
+    echo "File or directory `$1` does not exist." 1>&2; exit 1
 fi
 
