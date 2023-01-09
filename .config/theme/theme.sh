f8db1300157d6b1dda66a32a7d0fdf61887da16d
Casper <draakie@protonmail.com>
2023-01-09 18:38:22 +0100

    theme: Expand shell theme API with ANSI sequences.

--- .config/theme/theme.sh
+++ .config/theme/theme.sh
@@ -10,13 +10,28 @@
 source $XDG_CONFIG_HOME/theme/colors
 source $XDG_CONFIG_HOME/theme/fonts
 
-# Usage: `"$(colors_zerox $colorname)"`
+# Usage: `$(colors_zerox $colorname)`
 colors_hash () {
     echo -n \#${1:1}
 }
 
-# Usage: `"$(colors_zerox $colorname)"`
+# Usage: `$(colors_zerox $colorname)`
 colors_zerox () {
     echo -n 0x${1:1}
+}
+
+# Usage: `$(colors_ansi $colorfg $colorbg)`
+colors_ansi () {
+    $HOME/.scripts/color-hex-to-ansi.sh --fg=${1:1} --bg=${2:1}
+}
+
+# Usage: `$(colors_ansi_fg $colorfg)`
+colors_ansi_fg () {
+    $HOME/.scripts/color-hex-to-ansi.sh --fg=${1:1}
+}
+
+# Usage: `$(colors_ansi_reset)`
+colors_ansi_reset () {
+    $HOME/.scripts/color-hex-to-ansi.sh --reset
 }
 
