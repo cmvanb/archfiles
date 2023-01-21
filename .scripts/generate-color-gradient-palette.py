7a77e5732237f3abc4fb13b4765669f61d31b85b
Casper <draakie@protonmail.com>
2023-01-21 23:36:45 +0100

    scripts: Improve gradient palette generator; now correctly blends middle color with any midpoint.

--- .scripts/generate-color-gradient-palette.py
+++ .scripts/generate-color-gradient-palette.py
@@ -30,6 +30,13 @@ class Color():
     def __str__(self):
         return f'{self.r}, {self.g}, {self.b}'
 
+    # TODO: Find a better way to calculate a high contrast foreground color.
+    # see: https://stackoverflow.com/questions/9733288/how-to-programmatically-calculate-the-contrast-ratio-between-two-colors
+    # see: https://stackoverflow.com/questions/7260989/how-to-pick-good-contrast-rgb-colors-programmatically
+    # see: https://contrast-ratio.com/
+    def escape_fg(self):
+        return f'\x1b[38:2:{255 - self.r}:{255 - self.g}:{255 - self.b}m'
+
     def escape_bg(self):
         return f'\x1b[48:2:{self.r}:{self.g}:{self.b}m'
 
@@ -37,25 +44,26 @@ class Color():
         return '\x1b[0m'
 
     def terminal_output(self):
-        return f'{self.escape_bg()}#{self.r:02x}{self.g:02x}{self.b:02x}{self.escape_reset()}'
+        return f'{self.escape_bg()}{self.escape_fg()}#{self.r:02x}{self.g:02x}{self.b:02x}{self.escape_reset()}'
 
     @staticmethod
-    def parse(arg):
-        return Color(int(arg[1:3], 16), int(arg[3:5], 16), int(arg[5:7], 16))
-
-def validate_color(argumentName, argument):
-    if not argument:
-        raise ValueError(f'{argumentName} cannot be an empty string.')
-    if len(argument) != 7 or argument[:1] != '#':
-        raise ValueError(f'{argumentName} must be in the format #RRGGBB.')
-    return True
+    def parse(argument):
+        if not argument:
+            raise ValueError(f'Color cannot be an empty string.')
+        if len(argument) != 7 or argument[:1] != '#':
+            raise ValueError(f'Color must be in the format #RRGGBB.')
+        return Color(int(argument[1:3], 16), int(argument[3:5], 16), int(argument[5:7], 16))
 
 def interp(start, end, percentage):
     return start + ((end - start) * percentage)
 
+def interp_color(start, end, percentage):
+    return Color(interp(start.r, end.r, percentage), interp(start.g, end.g, percentage), interp(start.b, end.b, percentage))
+
 def main():
     import argparse
     import sys
+    import math
 
     try:
         parser = argparse.ArgumentParser(description='Generate color gradient palette')
@@ -67,35 +75,49 @@ def main():
 
         args = parser.parse_args()
 
-        validate_color('start', args.start)
-        validate_color('end', args.end)
+        startColor = Color.parse(args.start)
+        endColor = Color.parse(args.end)
         if args.middle is not None:
-            validate_color('middle', args.middle)
+            middleColor = Color.parse(args.middle)
 
     except Exception as ex:
         print(f'Error: {ex}')
         sys.exit(1)
 
-    # TODO: Implement midpoint.
+    maxIndex = args.samples - 1
+
     if args.middle is not None:
-        paletteA = generate(args.samples // 2, Color.parse(args.start), Color.parse(args.middle))
+        if args.midpoint is None:
+            midPoint = 0.5
+        else:
+            midPoint = args.midpoint
 
-        for i, color in enumerate(paletteA):
-            print(f'{color.terminal_output()}')
+        # Sample the colors to the left and right of the midpoint.
+        midLeftIndex = math.floor(midPoint * maxIndex)
+        endPointA = (midLeftIndex / maxIndex) / midPoint
+        endColorA = interp_color(startColor, middleColor, endPointA)
+        midRightIndex = math.ceil(midPoint * maxIndex)
+        startPointB = ((midRightIndex / maxIndex) - midPoint) / (1.0 - midPoint)
+        startColorB = interp_color(middleColor, endColor, startPointB)
 
-        paletteB = generate(args.samples // 2, Color.parse(args.middle), Color.parse(args.end))
+        # Then we can correctly sample the remaining colors.
+        palette = []
+        for index in range(0, args.samples):
+            samplePercentage = index / maxIndex
+            if samplePercentage < midPoint:
+                subSamplePercentage = samplePercentage / midPoint
+                palette.append(interp_color(startColor, endColorA, subSamplePercentage))
+            else:
+                subSamplePercentage = (samplePercentage - midPoint) / (1.0 - midPoint)
+                palette.append(interp_color(startColorB, endColor, subSamplePercentage))
 
-        for i, color in enumerate(paletteB):
-            print(f'{color.terminal_output()}')
     else:
-        palette = generate(args.samples, Color.parse(args.start), Color.parse(args.end))
+        palette = []
+        for index in range(0, args.samples):
+            palette.append(interp_color(startColor, endColor, index / maxIndex))
 
-        for i, color in enumerate(palette):
-            print(f'{color.terminal_output()}')
-
-def generate(samples, start, end):
-    div = samples - 1
-    return [Color(interp(start.r, end.r, (i / div)), interp(start.g, end.g, (i / div)), interp(start.b, end.b, (i / div))) for i in range(0, samples)]
+    for color in palette:
+        print(f'{color.terminal_output()}')
 
 if __name__ == '__main__':
     main()
