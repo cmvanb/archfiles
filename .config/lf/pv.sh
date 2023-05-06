196da5d0afe412feae4008d10b5957b88e0e70be
Casper <draakie@protonmail.com>
2023-05-06 02:20:47 +0200

    lf: Image previews with chafa.

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -7,6 +7,7 @@ set -o nounset
 set -o pipefail
 
 case "$1" in
+    *.jpg|*.jpeg|*.png) chafa "$1" -f sixel -s "$(($2-2))x$3" | sed 's/#/\n#/g' ;;
     *) bat --force-colorization --paging=never --style=changes,numbers \
             --wrap never -f "$1" && false ;;
 esac
