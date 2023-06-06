d2e19140dfbaf52a2c1daaa0f75e26a66b9a4b5d
Casper <draakie@protonmail.com>
2023-06-06 10:58:32 +0200

    waylock: Tweak lock screen input color.

--- .config/river/lock.sh
+++ .config/river/lock.sh
@@ -6,5 +6,5 @@
 
 source "$XDG_CONFIG_HOME/theme/theme.sh"
 
-waylock -init-color $(color_zerox $black) -input-color $(color_zerox $primary_6) -fail-color $(color_zerox $d1_red)
+waylock -init-color $(color_zerox $black) -input-color $(color_zerox $primary_12) -fail-color $(color_zerox $d1_red)
 
