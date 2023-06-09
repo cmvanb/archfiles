70d478e2ed97e4b21ec3c1c159bf082f859f2074
Casper <draakie@protonmail.com>
2023-06-09 22:12:39 +0200

    scripts: Add display rotation script.

--- .scripts/wl-get-outputs.sh
+++ .scripts/wl-get-outputs.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
-# Fetch password from Bitwarden
+# Get all wayland outputs.
 #-------------------------------------------------------------------------------
 
 set -o nounset
 set -o pipefail
 
-wlr-randr --output DP-3 --transform 90
-wlr-randr --output DP-3 --pos 0,0
-
-wlr-randr --output DP-2 --transform normal
-wlr-randr --output DP-2 --pos 1200,0
-
+wlr-randr | grep -v '^ ' | awk '{print $1}'
