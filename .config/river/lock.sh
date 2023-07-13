d62d81a4edbefb4dd1d400125b4dae629aad1b82
Casper <draakie@protonmail.com>
2023-07-13 10:50:18 +0200

    waylock: Emit notification if screen locker fails.

--- .config/river/lock.sh
+++ .config/river/lock.sh
@@ -8,3 +8,7 @@ source "$XDG_CONFIG_HOME/theme/theme.sh"
 
 waylock -init-color $(color_zerox $gray_0) -input-color $(color_zerox $primary_12) -fail-color $(color_zerox $red_4)
 
+if [ $? != 0 ]; then
+    notify-send "Failed to lock screen. Please see \`.local/share/river/session_log\` for details."
+fi
+
