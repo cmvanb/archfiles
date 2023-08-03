598bda952ffe246641c839765bfe8603f362bb7b
Casper <draakie@protonmail.com>
2023-08-03 14:50:54 +0200

    scripts: Add markdown-on-changed script, runs markdown script whenever file is changed.

---
+++ .scripts/markdown-on-changed.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ "$#" -ne 1 ]; then
+    echo "Expected only one argument: the filename of the markdown file. Received $# arguments."
+    exit 1
+fi
+
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+
+if [ "$extension" != "md" ]; then
+    echo "Expected markdown file with \`md\` extension. Received file with \`$extension\` extension."
+    exit 1
+fi
+
+echo $1 | entr -r $HOME/.scripts/markdown.sh $1
